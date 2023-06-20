900440d51dcec520e59583f668d04d0d6430d850
George@0810 <jacobgeorge399@gmail.com>
2023-06-20 20:24:30 +0530

    update

--- js/app.js
+++ js/app.js
@@ -8,14 +8,7 @@ const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
 
-footer_input.addEventListener("focus", () => {
-  footer_input.classList.add("focus");
-});
 
-footer_input.addEventListener("blur", () => {
-  if (footer_input.value != "") return;
-  footer_input.classList.remove("focus");
-});
 
 function closeMenu() {
   navbar.classList.remove("open");
@@ -44,11 +37,7 @@ filter_btns.forEach((btn) =>
   })
 );
 
-$(".grid").isotope({
-  itemSelector: ".grid-item",
-  layoutMode: "fitRows",
-  transitionDuration: "0.6s",
-});
+
 
 window.addEventListener("scroll", () => {
   skillsEffect();
@@ -98,4 +87,18 @@ var mySwiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
     nextEl: ".swiper-button-next",
   },
+});
+footer_input.addEventListener("focus", () => {
+  footer_input.classList.add("focus");
+});
+
+footer_input.addEventListener("blur", () => {
+  if (footer_input.value != "") return;
+  footer_input.classList.remove("focus");
+});
+
+$(".grid").isotope({
+  itemSelector: ".grid-item",
+  layoutMode: "fitRows",
+  transitionDuration: "0.6s",
 });
